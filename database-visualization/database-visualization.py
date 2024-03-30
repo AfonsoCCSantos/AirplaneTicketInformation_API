d46b9e6536fa540b0f5bbb12c7e10104a137e992
AfonsoCCSantos <afonso.acc.santos@protonmail.com>
2024-03-30 23:15:06 +0000

    added skeleton for database-visualization

--- database-visualization/database-visualization.py
+++ database-visualization/database-visualization.py
@@ -1,11 +1,50 @@
-from flask import Flask
+import grpc
+from concurrent import futures
+from grpc_interceptor import ExceptionToStatusInterceptor
+from grpc_interceptor.exceptions import NotFound
 
-app = Flask(__name__)
+from protobufs.compiled.visualization_pb2 import (
+    Ticket,
+    Airline,
+    TicketsResponse,
+    AirlineResponse,
+)
+import protobufs.compiled.visualization_pb2_grpc as visualization_pb2_grpc
 
-@app.route("/api/visualization/tickets/<departure>/<arrival>", method=["GET"])
-def get_tickets_from_to(departure, arrival):
-    return "list of tickets"
+class DatabaseVisualizationService(visualization_pb2_grpc.VisualizationServicer):
+    def GetTickets(self, request, context):
+        
+        return TicketsResponse(tickets=None)
+        
+    def GetAirline(self, request, context):
 
-@app.route("/api/visualization/airlines/<airline_code>", method=["GET"])
-def get_airline_details(airline_code):
-    return "list of tickets"
+        return AirlineResponse(airline = None)
+        
+def serve():
+    interceptors = [ExceptionToStatusInterceptor()]
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
+    )
+    visualization_pb2_grpc.add_RecommendationsServicer_to_server(
+        DatabaseVisualizationService(), server
+    )
+
+    # with open("server.key", "rb") as fp:
+    #     server_key = fp.read()
+    # with open("server.pem", "rb") as fp:
+    #     server_cert = fp.read()
+    # with open("ca.pem", "rb") as fp:
+    #     ca_cert = fp.read()
+
+    # creds = grpc.ssl_server_credentials(
+    #     [(server_key, server_cert)],
+    #     root_certificates=ca_cert,
+    #     require_client_auth=True,
+    # )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
