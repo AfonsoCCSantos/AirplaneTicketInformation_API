c414b684ed6550bc319b09a4df0de8feb90cacc1
AfonsoCCSantos <afonso.acc.santos@protonmail.com>
2024-03-31 10:19:39 +0000

    implemented GetAirlinesRankingByTicketPrice in ranking service

---
+++ database-ranking/database-ranking.py
@@ -0,0 +1,74 @@
+import grpc
+from concurrent import futures
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from grpc_interceptor import ExceptionToStatusInterceptor
+from grpc_interceptor.exceptions import NotFound
+
+from ranking_pb2 import (
+    AirlineRankingRow,
+    AirlinesRankingByTicketPriceResponse,
+)
+import ranking_pb2_grpc as ranking_pb2_grpc
+
+class DatabaseRankingService(ranking_pb2_grpc.RankingServicer):
+    global client 
+    
+
+    def GetAirlinesRankingByTicketPrice(self, request, context):
+        airlines = []
+
+        query = f"""
+            SELECT airlineCode, AVG(totalFare) as averagePrice
+            FROM ranking.ranking r
+            GROUP BY airlineCode
+            ORDER BY averagePrice
+        """
+
+        query_job = client.query(query)
+        results = query_job.result()
+
+        
+
+        for row in results:
+            airline = AirlineRankingRow(
+                airline_code=row.airlineCode,
+                average_price=row.averagePrice
+            )
+
+            airlines.append(airline)
+        
+        return AirlinesRankingByTicketPriceResponse(airlines=airlines)
+
+
+
+def serve():
+    interceptors = [ExceptionToStatusInterceptor()]
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
+    )
+    ranking_pb2_grpc.add_RankingServicer_to_server(
+        DatabaseRankingService(), server
+    )
+
+    # with open("server.key", "rb") as fp:
+    #     server_key = fp.read()
+    # with open("server.pem", "rb") as fp:
+    #     server_cert = fp.read()
+    # with open("ca.pem", "rb") as fp:
+    #     ca_cert = fp.read()
+
+    # creds = grpc.ssl_server_credentials(
+    #     [(server_key, server_cert)],
+    #     root_certificates=ca_cert,
+    #     require_client_auth=True,
+    # )
+    server.add_insecure_port("[::]:50052")
+    server.start()
+    server.wait_for_termination()
+
+client = None
+if __name__ == "__main__":
+    credentials = service_account.Credentials.from_service_account_file(filename="ranking_key.json")
+    client = bigquery.Client(credentials=credentials)
+    serve()
--- ranking/ranking.py
+++ ranking/ranking.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import grpc
+import os
+
+from ranking_pb2 import AirlinesRankingByTicketPriceRequest, AirlinesRankingByTicketPriceResponse, AirlineRankingRow
+from ranking_pb2_grpc import RankingStub
 
 app = Flask(__name__)
 
+# Connect to the database visualization service
+database_ranking_host = os.getenv("DATABASE_RANKING_HOST", "localhost")
+database_ranking_channel = grpc.insecure_channel("localhost:50052")
+database_ranking_client = RankingStub(database_ranking_channel)
+
 @app.route("/api/ranking/airlines_by_ticket_price", methods=['GET'])
 def get_ranking_airlines_ticket_pricing():
-    return "list of airlines ranked by ticket price"
+    airline_ranking_request = AirlinesRankingByTicketPriceRequest()
+
+    airline_ranking_response = database_ranking_client.GetAirlinesRankingByTicketPrice(airline_ranking_request)
+    
+    return f"ranking of airlines: {airline_ranking_response.airlines}"
