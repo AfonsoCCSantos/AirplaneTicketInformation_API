0f3ac26c47b7b0871e4f741b2d69b074142f7064
AnotherRaquelDomingos <raq.domingos@gmail.com>
2024-05-16 11:24:52 +0000

    added counter prometheus metrics to visualization and ranking services

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -1,9 +1,12 @@
 from flask import Flask
 import grpc
 import os
+import time
+import random
 
 from ranking_pb2 import AirlinesRankingByTicketPriceRequest, AirlineAveragePrice
 from ranking_pb2_grpc import RankingStub
+from prometheus_client import start_http_server, Summary, Histogram, CONTENT_TYPE_LATEST, generate_latest, Counter
 
 app = Flask(__name__)
 
@@ -11,9 +14,11 @@ app = Flask(__name__)
 database_ranking_host = os.getenv("DATABASE_RANKING_HOST", "localhost")
 database_ranking_channel = grpc.insecure_channel(f"{database_ranking_host}:50052")
 database_ranking_client = RankingStub(database_ranking_channel)
+request_counter = Counter("requests_counter_ranking", "Total number of requests of ranking")
 
 @app.route("/api/ranking/airlines_by_ticket_price", methods=['GET'])
 def get_ranking_airlines_ticket_pricing():
+    request_counter.inc(1)
     airline_ranking_request = AirlinesRankingByTicketPriceRequest()
 
     airline_ranking_response = database_ranking_client.GetAirlinesRankingByTicketPrice(airline_ranking_request)
@@ -23,3 +28,7 @@ def get_ranking_airlines_ticket_pricing():
 @app.route("/api/ranking/liveness-check", methods=['GET'])
 def liveness_check():
     return "ok",200
+
+@app.route("/metrics", methods=['GET'])
+def prometheus_metrics():
+    return generate_latest()
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -6,7 +6,7 @@ import random
 
 from visualization_pb2 import Ticket, TicketsRequest, Airline, AirlineRequest
 from visualization_pb2_grpc import VisualizationStub
-from prometheus_client import start_http_server, Summary, Histogram, CONTENT_TYPE_LATEST, generate_latest
+from prometheus_client import start_http_server, Summary, Histogram, CONTENT_TYPE_LATEST, generate_latest, Counter
 
 app = Flask(__name__)
 
@@ -14,13 +14,13 @@ app = Flask(__name__)
 database_visualization_host = os.getenv("DATABASE_VISUALIZATION_HOST", "localhost")
 database_visualization_channel = grpc.insecure_channel(f"{database_visualization_host}:50051")
 database_visualization_client = VisualizationStub(database_visualization_channel)
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-histogram = Histogram('python_my_histogram', 'This is my histogram')
+request_counter = Counter("requests_counter_visualization", "Total number of requests of visualization")
+
 ##start_http_server(9050)
 
-@REQUEST_TIME.time()
 @app.route("/api/visualization/tickets/<departure>/<arrival>", methods=["GET"])
 def get_tickets_from_to(departure, arrival):
+    request_counter.inc(1)
     tickets_request = TicketsRequest(
         departure_place=departure,
         arrival_place=arrival
@@ -30,16 +30,15 @@ def get_tickets_from_to(departure, arrival):
 
     return f"list of tickets: {tickets_response.tickets}"
 
-@REQUEST_TIME.time()
 @app.route("/api/visualization/airlines/<airline_code>", methods=["GET"])
 def get_airline_details(airline_code):
+    request_counter.inc(1)
     airline_request = AirlineRequest(airline_code=airline_code)
     
     airline_response = database_visualization_client.GetAirline(airline_request)
 
     return f"airlineCode: {airline_response.airline.airline_code} | airlineName: {airline_response.airline.airline_name}"
 
-@REQUEST_TIME.time()
 @app.route("/api/visualization/liveness-check", methods=['GET'])
 def liveness_check():
     rValue = random.random()
