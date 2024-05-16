54b7c603c52544c07fa4ee7f377c3d04bd27b00b
AnotherRaquelDomingos <raq.domingos@gmail.com>
2024-05-16 12:27:49 +0000

    added counter prometheus metric for forecast service

--- forecast/forecast.py
+++ forecast/forecast.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import time
+import random
+
+from prometheus_client import start_http_server, Summary, Histogram, CONTENT_TYPE_LATEST, generate_latest, Counter
 
 app = Flask(__name__)
 
+request_counter = Counter("requests_counter_forecast", "Total number of requests of forecast")
+
 @app.route("/api/forecast/chepeast/<departure>/<arrival>/<start_date>/<end_date>", methods=['GET'])
 def forecast_cheapest(departure, arrival, start_date, end_date):
+    request_counter.inc(1)
     return "forecast of cheapest ticket"
 
 @app.route("/api/forecast/liveness-check", methods=['GET'])
 def liveness_check():
     return "ok",200
+
+@app.route("/metrics", methods=['GET'])
+def prometheus_metrics():
+    return generate_latest() 
