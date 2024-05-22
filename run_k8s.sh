079fe40304d2453279173f75cc3b5496b034c6e6
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-05-22 10:38:48 +0000

    Recommendations and forecast services running in the cloud

--- run_k8s.sh
+++ run_k8s.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./create-secrets.sh
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
 kubectl wait --namespace ingress-nginx \
  --for=condition=ready pod \
