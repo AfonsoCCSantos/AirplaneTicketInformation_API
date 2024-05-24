6effd461fd895bcc3154409ade98e3cfe5373126
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-05-24 22:32:35 +0000

    added script to stop docker containers

---
+++ stop_containers.sh
@@ -0,0 +1,8 @@
+docker stop database-ranking-container
+docker stop database-ranking-container
+docker stop forecast-container
+docker stop management-container
+docker stop ranking-container
+docker stop recommendations-container
+docker stop visualization-container
+docker stop authentication-container
