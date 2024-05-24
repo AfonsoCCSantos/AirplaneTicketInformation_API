6bb83c12744b442923b146b67d5b2af65db2d4ee
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-05-24 21:51:38 +0000

    script to start up kubernetes

--- run_k8s.sh
+++ run_k8s.sh
@@ -2,10 +2,12 @@
 
 ./create-secrets.sh
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
+# kubectl get pods --namespace=ingress-nginx
 kubectl wait --namespace ingress-nginx \
  --for=condition=ready pod \
- --selector=app.kube
+ --selector=app.kubernetes.io/component=controller \
+ --timeout=120s
 kubectl apply -f ingress.yaml
 kubectl apply -f kubernetes.yaml
 kubectl apply -f autoscaler.yaml
-kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
+# kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
