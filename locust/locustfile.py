d7e4a282bad7019869c488a0c1b6214e42e94403
AfonsoCCSantos <afonso.acc.santos@protonmail.com>
2024-05-27 09:27:00 +0000

    added more tests to locust file

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -6,3 +6,7 @@ class HelloWorldUser(HttpUser):
     @task
     def view_airline(self):
         self.client.get("/api/visualization/airlines/DL")
+
+    @task
+    def view_ranking(self):
+        self.client.get("/api/ranking/airlines_by_ticket_price")
