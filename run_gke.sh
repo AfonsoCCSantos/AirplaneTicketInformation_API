10c5b2a8db741e0fa86b6ef8e9dbc1bf9145e9f0
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-06-12 21:15:19 +0000

    Changed cluster zone

--- run_gke.sh
+++ run_gke.sh
@@ -6,9 +6,9 @@ gcloud config set project disco-parsec-415719
 
 
 gcloud container clusters create cn19-cluster --region=europe-west4 \
-    --node-locations europe-west4-c \
+    --node-locations europe-west4-a \
     --num-nodes=5 \
-    --machine-type=custom-1-2048 # com5 nodes 1 vcpu e 2 de mem per node
+    --machine-type=custom-1-2048 # com 5 nodes 1 vcpu e 2 de mem per node
 
 
 gcloud container clusters get-credentials cn19-cluster --region=europe-west4 
@@ -18,4 +18,4 @@ kubectl config current-context
 ./run_k8s.sh
 #kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
 #disco-parsec-415719
-kubectl get ingress # obter ip
+#kubectl get ingress # obter ip
