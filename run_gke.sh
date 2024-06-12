0730f4e31ac56861894b53b2fd4d604ef12216ea
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-06-12 14:35:08 +0000

    Modified limits in k8s

--- run_gke.sh
+++ run_gke.sh
@@ -8,7 +8,7 @@ gcloud config set project disco-parsec-415719
 gcloud container clusters create cn19-cluster --region=europe-west4 \
     --node-locations europe-west4-c \
     --num-nodes=5 \
-    --machine-type=custom-1-2048 # com5 nodes 1 vcpu e 1,5 de mem?
+    --machine-type=custom-1-2048 # com5 nodes 1 vcpu e 2 de mem per node
 
 
 gcloud container clusters get-credentials cn19-cluster --region=europe-west4 
@@ -18,4 +18,4 @@ kubectl config current-context
 ./run_k8s.sh
 #kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
 #disco-parsec-415719
-# kubectl get ingress # obter ip
+kubectl get ingress # obter ip
--- stop_gke.sh
+++ stop_gke.sh
@@ -1,4 +1,3 @@
 ./stop_prometheus.sh
 ./stop_k8s.sh
 gcloud container clusters delete cn19-cluster --region=europe-west4 
-
